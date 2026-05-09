e01e7281c7e997670bb366efbb4c371d5a09fae9
Nouhaila Lançar <163061608+NOUHAILALANCAR@users.noreply.github.com>
2026-05-09 14:45:14 +0100

    Add checkpoint system

--- server/checkpoint.py
+++ server/checkpoint.py
@@ -1,1 +1,11 @@
+import torch
 
+
+def save_checkpoint(model, round_number):
+
+    torch.save({
+
+        "round": round_number,
+        "model": model.state_dict()
+
+    }, f"checkpoint_round_{round_number}.pth")
