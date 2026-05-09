0a84bbd1fadf00e296e14f89c2a6a3f8c492aa69
Nouhaila Lançar <163061608+NOUHAILALANCAR@users.noreply.github.com>
2026-05-09 14:44:31 +0100

    Add FedAvg aggregation

--- server/aggregation.py
+++ server/aggregation.py
@@ -1,1 +1,17 @@
 
+import copy
+
+
+def fedavg(client_updates):
+
+    avg_weights = copy.deepcopy(client_updates[0])
+
+    for key in avg_weights.keys():
+
+        for i in range(1, len(client_updates)):
+
+            avg_weights[key] += client_updates[i][key]
+
+        avg_weights[key] = avg_weights[key] / len(client_updates)
+
+    return avg_weights
