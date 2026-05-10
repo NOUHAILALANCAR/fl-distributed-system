38b02cc4cd5aed6db6c5ac16a678722a54fe6126
Nouhaila Lançar <163061608+NOUHAILALANCAR@users.noreply.github.com>
2026-05-10 14:15:43 +0100

    logger.py

--- monitoring/logger.py
+++ monitoring/logger.py
@@ -1,1 +1,7 @@
+from loguru import logger
+import sys
 
+logger.remove()
+logger.add(sys.stdout, format="<green>{time:HH:mm:ss}</green> | <level>{level}</level> | {message}",
+           colorize=True, level="INFO")
+logger.add("fl_system.log", rotation="10 MB", level="DEBUG")
