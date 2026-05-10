46fc5f27ec0cbb79c8e35638fee701bcbfd2a9b2
Nouhaila Lançar <163061608+NOUHAILALANCAR@users.noreply.github.com>
2026-05-10 17:27:59 +0100

    test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,1 +1,24 @@
+import unittest
+import time
+from server.fault_tolerance import FaultTolerance
 
+class TestFederatedLearning(unittest.TestCase):
+
+    def test_quorum(self):
+        ft = FaultTolerance(quorum=3)
+        self.assertTrue(ft.check_quorum(["c1", "c2", "c3"]))
+        self.assertFalse(ft.check_quorum(["c1", "c2"]))
+
+    def test_byzantine_detection(self):
+        ft = FaultTolerance()
+        # Simulation simple
+        fake_weights = {"layer1": 0}  # À adapter selon ta structure
+        self.assertIsInstance(ft.detect_byzantine(fake_weights, fake_weights), bool)
+
+    def test_backoff(self):
+        ft = FaultTolerance()
+        sleep_time = ft.exponential_backoff(3)
+        self.assertLessEqual(sleep_time, 8)
+
+if __name__ == "__main__":
+    unittest.main()
